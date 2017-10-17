99911d9c8e1837ca59e1439aa53873303733deb9
todbertuzzi <tod.bertuzzi@gmail.com>
2017-10-17 18:12:09 +0200

    Modfiche header
    Spostata navbar-brand fuori dalla navbar e aggiunto relativo stile

--- templates/header.php
+++ templates/header.php
@@ -3,8 +3,8 @@
 ?>
 
 <header class="banner" role="banner">
+ <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>      
  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
       <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         &#9776;
       </button>
