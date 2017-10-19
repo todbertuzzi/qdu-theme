b810fef53f5d94f941b8ce54595b0d0a971bccd8
todbertuzzi <tod.bertuzzi@gmail.com>
2017-10-19 21:11:42 +0200

    Modifiche Varie
    Aggiunto Snap.svg
    Modificato layout frontpage
    Aggiunto logo svg e animazioni

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,6 +19,8 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
+        //var draw = SVG('#qdu_barra_2')
+        //draw.animate().move(100, 100)
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
@@ -28,6 +30,168 @@
     'home': {
       init: function() {
         // JavaScript to be fired on the home page
+        
+       // qdu_university_logo.animate(500, '>', 400).attr({ opacity: '1' })
+        //var s = Snap("#shape");
+/*
+        var qdu_quasar_logo = SVG.get('qdu_quasar_svg')
+        qdu_quasar_logo.style('opacity', '0') 
+        qdu_quasar_logo.animate(1000, '>', 0).style('opacity', '1') 
+
+        var qdu_barra_1_logo = SVG.get('qdu_barra_1');
+        // qdu_design_logo.style('opacity', '0') 
+        var w_barra_1= qdu_barra_1_logo.width();
+        qdu_barra_1_logo.width(0) 
+        qdu_barra_1_logo.animate(200, '>', 500).width(w_barra_1) ;
+       
+
+        var qdu_design_logo = SVG.get('qdu_design_svg')
+        qdu_design_logo.style('opacity', '0') 
+        qdu_design_logo.animate(1000, '>', 300).style('opacity', '1')
+
+       
+        var qdu_barra_2_logo = SVG.get('qdu_barra_2');
+        // qdu_design_logo.style('opacity', '0') 
+        var w_barra_2= qdu_barra_2_logo.width();
+        qdu_barra_2_logo.width(0) 
+        qdu_barra_2_logo.animate(200, '>', 1000).width(w_barra_2) ;
+
+
+        var qdu_university_logo = SVG.get('qdu_university_svg')
+        qdu_university_logo.style('opacity', '0') 
+        qdu_university_logo.animate(1000, '>', 600).style('opacity', '1')  
+
+        var qdu_q_logo = SVG.get('qdu_q')
+        //qdu_q_logo.attr('stroke-dasharray', 1000)
+        //qdu_q_logo.attr('stroke-dashoffset', 1000)
+        //qdu_q_logo.attr('fill', '#ffffff')
+        //qdu_q_logo.attr('stroke', '#0095D5')
+        qdu_q_logo.animate(3000, '<', 1600).attr('stroke-dashoffset', 0). animate(500, '>', 0).attr('fill', '#ffffff') 
+
+        var  qdu_barra_q_logo = SVG.get('qdu_barra_q');
+        var w_barra_q= qdu_barra_q_logo.width();
+        qdu_barra_q_logo.width(0) 
+        qdu_barra_q_logo.animate(200, '>', 3000).width(w_barra_q) ;
+        
+        
+        var altezza = $(".home").height();
+ */       
+        shapeEl = $('div.morph-shape');
+
+
+        // RESET OPACITY LOGO
+        var qdu_quasar_logo =  new Snap('#qdu_quasar_svg')
+         qdu_quasar_logo.attr({
+          opacity: 0
+        }); 
+         var qdu_design_logo =  new Snap('#qdu_design_svg')
+         qdu_design_logo.attr({
+          opacity: 0
+        }); 
+         var qdu_university_logo =  new Snap('#qdu_university_svg')
+         qdu_university_logo.attr({
+          opacity: 0
+        });
+        // RESET BARRE WIDTH
+        var qdu_barra_1_logo = new Snap('#qdu_barra_1');
+        var w_barra_1=  qdu_barra_1_logo.attr( 'width' )
+        qdu_barra_1_logo.attr({
+          width: 0
+        });         
+        var qdu_barra_2_logo = new Snap('#qdu_barra_2');
+        var w_barra_2=  qdu_barra_2_logo.attr( 'width' )
+        qdu_barra_2_logo.attr({
+          width: 0
+        }); 
+        // RESET BARRA SOTTO Q
+        var qdu_barra_q_logo = new Snap('#qdu_barra_q');
+        var w_barra_q=  qdu_barra_q_logo.attr( 'width' )
+        qdu_barra_q_logo.transform( 'r180' ); 
+        qdu_barra_q_logo.attr({
+          width: 0
+        });  
+        
+
+        // COMPARSA SCRITTE
+        setTimeout(function () {
+          qdu_quasar_logo.animate({ "opacity" : 1 }, 500, mina.easeout );
+        }, 100);
+        setTimeout(function () {
+          qdu_design_logo.animate({ "opacity" : 1 }, 500, mina.easeout );
+        }, 400);
+        setTimeout(function () {
+          qdu_university_logo.animate({ "opacity" : 1 }, 500, mina.easeout );
+        }, 700);
+        
+        // ANIMAZIONI BARRE
+        setTimeout(function () {
+           qdu_barra_1_logo.animate({ width : w_barra_1 }, 500, mina.easeinout );       
+        }, 1200);
+        setTimeout(function () {
+          qdu_barra_2_logo.animate({ width : w_barra_2 }, 500, mina.easeinout );
+        }, 1200);
+        
+        // DRAWING Q LOGO
+        var qdu_q_logo = new Snap('#qdu_q');
+        qdu_q_logo.animate({ "stroke-dashoffset" : 0 }, 2000, mina.easeout );
+        // CAMBIO COLORE Q
+        setTimeout(function () {
+          qdu_q_logo.animate({ "fill" : "#ffffff" }, 500, mina.easeinout );
+        }, 1400);
+        // ANIMAZIONE BARRA Q
+        setTimeout(function () {
+          qdu_barra_q_logo.animate({ width : w_barra_q }, 500, mina.easeinout );
+        }, 1200);
+        
+        // ANIMAZIONE MASCHERA CONTENUTI
+        //var s = Snap( shapeEl.querySelector( 'svg' ) );
+        var s = new Snap('div.morph-shape svg')
+        var pathEl = s.select( 'path' );
+        //console.log("shapeEl "+shapeEl);
+        var morph_1 = shapeEl.attr( 'data-morph-1' )
+        var morph_2 = shapeEl.attr( 'data-morph-2' )
+        var morph_3 = shapeEl.attr( 'data-morph-3' )
+        //console.log("pathEl "+pathEl);
+        //console.log("pathOpen "+pathOpen);
+        var pathReset = pathEl.attr( 'd' );
+        
+       // s.transform( 'r180' ); 
+
+
+        setTimeout(function () {
+          pathEl.animate({ d:morph_1 }, 500, mina.easeinout, fase_2);
+        }, 1800);
+
+        function fase_2(){
+          pathEl.animate({ d:morph_2 }, 800, mina.easeout,nascondoSvg);
+          console.log("fase_2")
+        }
+        function fase_3(){
+            //pathEl.animate({ d:morph_3 }, 500, mina.easeout,nascondoSvg);
+            console.log("fase_3")
+        }
+        function nascondoSvg(){
+                  $('div.morph-shape').hide();
+        }
+/*
+        pathEl.stop().animate( { 'path' : pathOpen }, 320, mina.easeinout, function() {
+          pathEl.stop().animate( { 'path' : pathReset }, 1000, mina.elastic );
+        } );
+       
+
+        $('#wrapperMask').css({
+            'transform-origin': 'center top',
+            '-webkit-transform-origin': 'center bottom'
+        });
+
+        $( "#wrapperMask" ).stop(true, true).delay(1000).animate({
+          //left: "+=50",
+          top: - (altezza+100)
+        }, 3000,"linear", function() {
+         console.log("daie");
+        });
+ */       
+         
       },
       finalize: function() {
         // JavaScript to be fired on the home page, after the init JS
--- templates/header-frontpage.php
+++ templates/header-frontpage.php
@@ -1,24 +1,32 @@
 <?php
   // For use with Sagextras (https://github.com/storm2k/sagextras)
 ?>
-<a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php get_template_part('templates/svg/inline','qdu_logo_banner.svg'); ?></a>      
+<a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><div class="container testata"><?php get_template_part('templates/svg/inline','qdu_logo_banner.svg'); ?></div></a>     
 <!--<div id="wrapperMask"></div>-->
 <header class="banner" role="banner">
+ <div class="container">
  <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         &#9776;
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
-          <a class="shiftnav-toggle shiftnav-toggle-button" data-shiftnav-target="shiftnav-main"><i class="fa fa-bars"></i>Menu</a>
+          <a class="shiftnav-toggle shiftnav-toggle-button" data-shiftnav-target="shiftnav-main"><i class="fa fa-bars"></i>MENU</a>
           <?php get_search_form(true); ?>
           <?php
           if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'pull-md-right nav navbar-nav']);
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'pull-md-right nav navbar-nav ']);
           endif;
           ?>
       </div>
     </nav>
 </header>
+</div>
+
+<div class="morph-shape"  data-morph-1="M0,0L0,400 550,400 550,0Q273.19,210.13 0,0" data-morph-2="M0,381.95L0,400 550,400 550,381.95 0,381.95" data-morph-3="M550.45,108L550.45,0 0.45,0 0.45,175Q84.98,208.36 274.65,224.45 440.34,238.67 550.45,1">
+              <svg width="100%" height="100%"  transform="scale(1,-1)" viewBox="0 0 550 400" preserveAspectRatio="none">
+                <path  fill="none" d="M0,0L0,400 550,400 550,0 0,0"/>
+              </svg>
+</div>
 
 <!--
 <div class="shape-wrap">
